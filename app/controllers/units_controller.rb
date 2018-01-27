ea22b3ef8db9f05042ba379ccde8e934c0e8aa6a
Eric Loos <ericloos00@gmail.com>
2018-01-26 20:50:21 -0600

    create units functions and view setup

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -4,5 +4,29 @@ class UnitsController < ApplicationController
     render json: @units
   end
 
+  def new
+    @unit = Unit.new
+    @units = Unit.all
+  end
+
+  def create
+    @unit = Unit.new(unit_params)
+    if @unit.save
+      redirect_to new_unit_path
+    else
+      render json: { errors: @unit.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+  private
+
+
+  def unit_params
+    params.require(:unit).permit(:name, :cleaning_time)
+  end
+
 
 end
